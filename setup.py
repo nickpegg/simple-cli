3e175775bbeb24e35cefe5ea7f70076587fe84d8
Nick Pegg <nick@nickpegg.com>
2014-08-30 00:10:35 -0400

    Remove old requirements

--- setup.py
+++ setup.py
@@ -12,10 +12,8 @@ setup(
     description='A CLI tool for interacting with your Simple bank account',
     long_description=open('README.md').read(),
     install_requires=[
-        'docopt>=0.6.2',
         'clint>=0.3.7',
         'requests>=2.3.0',
-        'PyYAML>=3.11',
         'click==3.2',
     ],
 )
